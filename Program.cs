706ecca9fbd334e5e7694cadc253f455f7c9f5e4
Eda Öney <oneeyyeda@gmail.com>
2025-07-28 17:55:55 +0300

    Completed

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿
+
+Console.WriteLine("HESAPLAMA UYGULAMASI");
+
+
+Console.Write("1. sayıyı giriniz: " );  
+double number1 = int.Parse(Console.ReadLine());
+ 
+ 
+Console.Write("2. sayıyı giriniz: ");
+double number2 = int.Parse(Console.ReadLine());
+ 
+Console.Write("İşlem seçiniz ( +, -, *, /): ");
+string islem = Console.ReadLine();
+
+double sonuc;
+
+if (islem == "+")
+{
+    sonuc = number1 + number2;
+    Console.WriteLine(sonuc);
+}
+
+if (islem == "-")
+{
+    sonuc = number1 - number2;
+    Console.WriteLine(sonuc);
+}
+
+if (islem == "*")
+{
+    sonuc = number1 * number2;
+    Console.WriteLine(sonuc);
+}
+
+if (islem == "/")
+{
+    sonuc = number1 / number2;
+    Console.WriteLine(sonuc);
+}
